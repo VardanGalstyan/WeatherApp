e00991ae9e036296a51e89298bfbd8eb68e4d48c
VardanGalstian <81989128+VardanGalstyan@users.noreply.github.com>
2021-12-27 21:54:15 +0100

    enhancers check

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -5,6 +5,7 @@ import thunk from "redux-thunk"
 import currentDataBaseReducer from "../reducers/currentReducer.js"
 import queryModalReducer from "../reducers/QueryModalReducer.js"
 
+
 export const initialState = {
 
     weather: {
@@ -40,7 +41,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const configureStore = createStore(
     combinedReducers,
     initialState,
-    process.env.REACT_APP_DEVELOPMENT ? composeEnhancers(applyMiddleware(thunk)) : compose(applyMiddleware(thunk))
+    process.env.REACT_APP_DEVELOPMENT ?  compose(applyMiddleware(thunk)) : composeEnhancers(applyMiddleware(thunk))
 )
 
 export default configureStore
